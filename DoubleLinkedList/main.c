c3a25dc4b0b96f98dac697ff92fd789f5f75e2bd
chenyufeng <503434355@qq.com>
2016-02-27 18:46:24 +0800

    Initial Commit

---
+++ DoubleLinkedList/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  DoubleLinkedList
+//
+//  Created by chenyufeng on 16/2/27.
+//  Copyright © 2016年 chenyufengweb. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
