4d7318e6319dc996b08da0c06b62ab8ce503458a
chenyufeng <503434355@qq.com>
2016-02-27 19:46:26 +0800

    Initial Commit

--- DoubleLinkedList/main.c
+++ DoubleLinkedList/main.c
@@ -6,10 +6,104 @@
 //  Copyright © 2016年 chenyufengweb. All rights reserved.
 //
 
+/**
+ *  实现一个非循环双向链表
+ */
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef int elemType;
+typedef struct NodeList{
+
+    int element;
+    struct NodeList *prior;
+    struct NodeList *next;
+}Node;
+
+//初始化非循环双向链表
+void initList(Node *pNode){
+
+    pNode = NULL;
+    printf("%s函数执行，链表初始化完成\n",__FUNCTION__);
+}
+
+//打印非循环双向链表
+void printList(Node *pNode){
+
+    if (pNode == NULL) {
+        printf("%s函数执行，链表为空，打印失败\n",__FUNCTION__);
+    }else{
+        while (pNode != NULL) {
+            printf("%d ",pNode->element);
+            pNode = pNode->next;
+        }
+        printf("\n");
+    }
+}
+
+//创建非循环双向链表
+Node *createList(Node *pNode){
+
+    Node *pInsert;
+    Node *pMove;
+    pInsert = (Node*)malloc(sizeof(Node));
+    memset(pInsert, 0, sizeof(Node));
+    pInsert->next = NULL;
+    pInsert->prior = NULL;
+
+    scanf("%d",&(pInsert->element));
+    pMove = pNode;
+
+    if (pInsert->element <= 0) {
+
+        printf("%s函数执行，输入数据非法，建立链表停止\n",__FUNCTION__);
+        return NULL;
+    }
+
+    while (pInsert->element > 0) {
+        if (pNode == NULL) {
+            pNode = pInsert;
+            pMove = pNode;
+        }else{
+
+            pMove->next = pInsert;
+            pInsert->prior = pMove;
+            pMove = pMove->next;
+        }
+
+        pInsert = (Node *)malloc(sizeof(Node));
+        memset(pInsert, 0, sizeof(Node));
+        pInsert->next = NULL;
+        pInsert->prior = NULL;
+        scanf("%d",&(pInsert->element));
+    }
+
+    printf("%s函数执行，建立链表成功\n",__FUNCTION__);
+
+    return pNode;
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+
+    Node *pList;
+
+    initList(pList);
+    printList(pList);
+
+    pList = createList(pList);
+    printList(pList);
+
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
