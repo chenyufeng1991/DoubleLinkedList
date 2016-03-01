7452c273bfebf6eadcf219c4145de161ee74740d
chenyufeng <503434355@qq.com>
2016-03-01 22:12:22 +0800

    11种基本操作

--- DoubleLinkedList/main.c
+++ DoubleLinkedList/main.c
@@ -7,7 +7,7 @@
 //
 
 /**
- *  实现一个非循环双向链表
+ *  实现一个非循环双向链表（不带头结点）
  */
 
 #include <stdio.h>
@@ -22,28 +22,14 @@ typedef struct NodeList{
     struct NodeList *next;
 }Node;
 
-//初始化非循环双向链表
+//1.初始化不带头结点的非循环双向链表
 void initList(Node *pNode){
 
     pNode = NULL;
     printf("%s函数执行，链表初始化完成\n",__FUNCTION__);
 }
 
-//打印非循环双向链表
-void printList(Node *pNode){
-
-    if (pNode == NULL) {
-        printf("%s函数执行，链表为空，打印失败\n",__FUNCTION__);
-    }else{
-        while (pNode != NULL) {
-            printf("%d ",pNode->element);
-            pNode = pNode->next;
-        }
-        printf("\n");
-    }
-}
-
-//创建非循环双向链表
+//2.创建非循环双向链表
 Node *createList(Node *pNode){
 
     Node *pInsert;
@@ -81,8 +67,64 @@ Node *createList(Node *pNode){
     }
 
     printf("%s函数执行，建立链表成功\n",__FUNCTION__);
-
+    
     return pNode;
+}
+
+//3.打印非循环双向链表
+void printList(Node *pNode){
+
+    if (pNode == NULL) {
+        printf("%s函数执行，链表为空，打印失败\n",__FUNCTION__);
+    }else{
+        while (pNode != NULL) {
+            printf("%d ",pNode->element);
+            pNode = pNode->next;
+        }
+        printf("\n");
+    }
+}
+
+//4.清除线性表L中的所有元素，使之成为一个空表
+Node *clearList(Node *pNode){
+
+    if (pNode == NULL) {
+        printf("%s函数执行，原链表就是空链表，无须执行该方法\n",__FUNCTION__);
+        return  NULL;
+    }else{
+        while (pNode->next != NULL) {
+
+            //一次删除每一个节点
+            pNode = pNode->next;
+            free(pNode->prior);
+            pNode->prior = NULL;
+        }
+
+        //清除最后一个节点
+        free(pNode);
+        pNode = NULL;
+
+        printf("%s函数执行，双向非循环链表清空成功\n",__FUNCTION__);
+        return pNode;
+    }
+}
+
+//5.返回不带头节点的双向链表的长度
+int sizeList(Node *pNode){
+
+    int i = 0;
+    if (pNode == NULL) {
+        printf("%s函数执行，链表为空，长度为0\n",__FUNCTION__);
+        return 0;
+    }else{
+
+        while (pNode != NULL) {
+            i++;
+            pNode = pNode->next;
+        }
+        printf("%s函数执行，链表长度为%d\n",__FUNCTION__,i);
+        return i;
+    }
 }
 
 int main(int argc, const char * argv[]) {
@@ -91,8 +133,13 @@ int main(int argc, const char * argv[]) {
 
     initList(pList);
     printList(pList);
+    sizeList(pList);
 
     pList = createList(pList);
+    printList(pList);
+    sizeList(pList);
+
+    pList = clearList(pList);
     printList(pList);
 
     return 0;
